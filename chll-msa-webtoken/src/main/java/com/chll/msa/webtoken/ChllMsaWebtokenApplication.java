63637216dadd3557f0ac2e02a7d0c68d19c92ad4
Oscar Sarcos <oscar.sarcoss@outlook.com>
2025-02-23 11:48:51 -0500

    commit initial

---
+++ chll-msa-webtoken/src/main/java/com/chll/msa/webtoken/ChllMsaWebtokenApplication.java
@@ -0,0 +1,13 @@
+package com.chll.msa.webtoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChllMsaWebtokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChllMsaWebtokenApplication.class, args);
+	}
+
+}
